76404f2bac71867c23b99a81e34ed95c9c58dd20
Yoan Blanc <yoan@dosimple.ch>
2019-12-27 18:30:57 +0100

    setup.py: read version without installing
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import os
 from setuptools import setup
 
-import editorconfig
+# Read the version
+g = {}
+with open(os.path.join("editorconfig", "version.py"), "rt") as fp:
+    exec(fp.read(), g)
+    v = g['VERSION']
+    version = ".".join((str(x) for x in v[:3]))
+    if v[3] != "final":
+        version += "-" + v[3]
 
 setup(
     name='EditorConfig',
-    version=editorconfig.__version__,
+    version=version,
     author='EditorConfig Team',
     packages=['editorconfig'],
     url='http://editorconfig.org/',
