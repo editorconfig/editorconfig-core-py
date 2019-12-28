614338eff9ba026c0f40ccab5fe9ffe4b9d55df2
Yoan Blanc <yoan@dosimple.ch>
2019-12-28 18:25:16 +0100

    Update setup.py
    Co-Authored-By: Hong Xu <hong@topbug.net>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ g = {}
 with open(os.path.join("editorconfig", "version.py"), "rt") as fp:
     exec(fp.read(), g)
     v = g['VERSION']
-    version = ".".join((str(x) for x in v[:3]))
+    version = ".".join(str(x) for x in v[:3])
     if v[3] != "final":
         version += "-" + v[3]
 
