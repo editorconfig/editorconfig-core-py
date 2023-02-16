40bf434278cf563e42fec08ae5f4459e8b8cf047
KOLANICH <KOLANICH@users.noreply.github.com>
2023-02-16 07:14:53 +0000

    Move the most of metadata into `setup.cfg` supported since Dec 2015. (#44)
    * Move the most of metadata into `setup.cfg` supported since Dec 2015.
    
    * Add `PEP 517`-compliant `pyproject.toml`

--- setup.py
+++ setup.py
@@ -10,35 +10,5 @@ with open(os.path.join("editorconfig", "version.py"), "rt") as fp:
     if v[3] != "final":
         version += "-" + v[3]
 
-setup(
-    name='EditorConfig',
-    version=version,
-    author='EditorConfig Team',
-    packages=['editorconfig'],
-    url='http://editorconfig.org/',
-    project_urls = {
-        "Source Code": "https://github.com/editorconfig/editorconfig-core-py",
-        "Bug Tracker": "https://github.com/editorconfig/editorconfig-core-py/issues",
-    },
-    license='python',
-    description='EditorConfig File Locator and Interpreter for Python',
-    long_description=open('README.rst').read(),
-    entry_points = {
-        'console_scripts': [
-            'editorconfig = editorconfig.__main__:main',
-        ]
-    },
-    classifiers=[
-        'License :: OSI Approved :: Python Software Foundation License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-)
+if __name__ == "__main__":
+    setup(version=version)
